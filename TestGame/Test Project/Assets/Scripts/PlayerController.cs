25c1013dd728ac15cc5f2b729ad1d2cfff76ea89
Daniel <daniel.f.wood@gmail.com>
2019-10-06 21:25:37 -0600

    Reorganized, added PlayerController script, added Camera Follow script
    At home, 10/6/2019

---
+++ TestGame/Test Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Original template taken from Unity Roll-A-Ball Tutorial: https://learn.unity.com/tutorial/environment-and-player?projectId=5c51479fedbc2a001fd5bb9f
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+}
