559e9a3917c1dda0a2a5153705d68b1e7e928a77
Daniel <daniel.f.wood@gmail.com>
2019-10-16 04:19:08 -0600

    Started modifying movement based on previous work, jump working
    At home, 10/16/2019

---
+++ TestGame/Test Project/Assets/Scripts/DGM-1610-Based/MoveCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+    private CharacterController controller;
+    public UnityEvent OnGrounded, OffGrounded;
+    //^AnimateNormalArm, AnimateMechanicalArm
+
+    public MoveBase CharacterMover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //NOT USED: AnimateNormalArm.Invoke();
+        //NOT USED: AnimateMechanicalArm.Invoke();
+        //Find a way to tie these to sprite animation, how to flip layer prominence and orientation based on character direction
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded) {
+            OnGrounded.Invoke();
+        } else {
+            OffGrounded.Invoke();
+        }
+
+        CharacterMover.Move(controller);
+
+
+    }
+}
